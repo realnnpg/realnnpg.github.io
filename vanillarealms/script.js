3fee421d917e39a2d64c0519c4a0870ebb579f75
nnpg <charis.giannitsaros@gmail.com>
2024-08-12 19:58:51 +0300

    online_players1

--- vanillarealms/script.js
+++ vanillarealms/script.js
@@ -63,3 +63,33 @@ var swiper = new Swiper('.swiper-container', {
         prevEl: '.swiper-button-prev',
     },
 });
+
+
+
+//online_players
+
+function initServerData(serverIp,serverPort){
+  const serverIpElement = document.getElementById('server-ip');
+  serverIpElement.innerHTML = serverIp;
+  fetch('https://mcapi.us/server/status?ip='+serverIp+'&port='+serverPort)
+  .then(response => response.json())
+  .then(data => handleServerStatus(data));
+
+  function handleServerStatus(data){
+      if(data.status=='error'){
+          console.log(data.error);
+          return false;
+      }
+      const motd = document.getElementById("motd");
+      motd.innerHTML = data.motd;
+
+      const playerCounter = document.getElementById("player-count");
+      playerCounter.innerHTML = data.players.now;
+
+      const logo = document.getElementById("server-icon");
+      logo.src = data.favicon;
+  } 
+}
+
+
+initServerData("play.vanillarealms.com","19132");
