f1d16440462cbe1e485a970137018ef3d927c4d7
nnpg <charis.giannitsaros@gmail.com>
2024-08-12 15:44:46 +0300

    photos3

--- vanillarealms/script.js
+++ vanillarealms/script.js
@@ -57,10 +57,7 @@ var swiper = new Swiper('.swiper-container', {
     slidesPerView: 3,
     spaceBetween: 30,
     loop: true,
-    pagination: {
-        el: '.swiper-pagination',
-        clickable: true,
-    },
+    pagination: false,
     navigation: {
         nextEl: '.swiper-button-next',
         prevEl: '.swiper-button-prev',
