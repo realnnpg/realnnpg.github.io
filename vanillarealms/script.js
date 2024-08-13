7dd09aa9879837c9656ac8e0144205a82d77f981
nnpg <charis.giannitsaros@gmail.com>
2024-08-13 18:59:32 +0300

    animations4?

--- vanillarealms/script.js
+++ vanillarealms/script.js
@@ -90,31 +90,3 @@ var swiper = new Swiper('.swiper-container', {
 });
 
 
-
-//ONLINE PLAYERS
-
-function initServerData(serverIp,serverPort){
-  const serverIpElement = document.getElementById('server-ip');
-  serverIpElement.innerHTML = serverIp;
-  fetch('https://mcapi.us/server/status?ip='+serverIp+'&port='+serverPort)
-  .then(response => response.json())
-  .then(data => handleServerStatus(data));
-
-  function handleServerStatus(data){
-      if(data.status=='error'){
-          console.log(data.error);
-          return false;
-      }
-      const motd = document.getElementById("motd");
-      motd.innerHTML = data.motd;
-
-      const playerCounter = document.getElementById("player-count");
-      playerCounter.innerHTML = data.players.now;
-
-      const logo = document.getElementById("server-icon");
-      logo.src = data.favicon;
-  } 
-}
-
-
-initServerData("play.vanillarealms.com","19132");
